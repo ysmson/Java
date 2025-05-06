aa086db11e2c48acb12c65b3391817606e3d8af8
ChengLun <ysmson@gmail.com>
2025-05-06 11:13:14 +0800

    202505061110

--- ShawnJava/src/tw/shawn/tutor/Shawn70.java
+++ ShawnJava/src/tw/shawn/tutor/Shawn70.java
@@ -1,12 +1,16 @@
-package tw.shawn.tutor;
+package tw.brad.tutor;
 
-public class Shawn70 {
+public class Brad70 {
 	public static void main(String[] args) {
 		Store2 store = new Store2();
 		Producer2 p = new Producer2(store);
-		Consumer2 c1 = new Consumer2(store);
+		Consumer2 c1 = new Consumer2(store, "A");
+		Consumer2 c2 = new Consumer2(store, "B");
+		Consumer2 c3 = new Consumer2(store, "C");
 		p.start();
 		c1.start();
+		c2.start();
+		c3.start();
 
 	}
 }
@@ -14,21 +18,25 @@ class Store2 {
 	private int qty;
 	private final int max = 10;
 	
-	void add(int add) {
+	synchronized void add(int add) throws InterruptedException {
 		System.out.printf("進貨中...%d\n", add);
 		while (add + qty > max) {
 			// wait....
+			wait();
 		}
 		qty += add;
+		notifyAll();
 		System.out.printf("完成進貨中...%d, 目前庫存量: %d\n", add, qty);
 	}
-	void buy(int buy) {
+	synchronized void buy(int buy,String who) throws InterruptedException {
 		System.out.printf("買貨中...%d\n", buy);
 		while (buy > qty) {
 			// wait....
+			wait();
 		}
 		qty -= buy;
-		System.out.printf("完成買貨中...%d, 目前庫存量: %d\n", buy, qty);
+		notifyAll();
+		System.out.printf("<%s>完成買貨中...%d, 目前庫存量: %d\n", who, buy, qty);
 	}
 }
 
@@ -40,8 +48,8 @@ class Producer2 extends Thread {
 	@Override
 	public void run() {
 		for (int i=0; i<10; i++) {
-			store.add(5);
 			try {
+				store.add(5);
 				Thread.sleep(200);
 			} catch (InterruptedException e) {
 			}
@@ -50,15 +58,18 @@ class Producer2 extends Thread {
 }
 class Consumer2 extends Thread {
 	private Store2 store;
-	public Consumer2(Store2 store) {
+	private String name;
+	
+	public Consumer2(Store2 store, String name) {
 		this.store = store;
+		this.name = name;
 	}
 	@Override
 	public void run() {
 		for (int i=0; i<10; i++) {
-			store.buy((int)(Math.random()*5)+1);
 			try {
-				Thread.sleep(300);
+				store.buy((int)(Math.random()*5)+1, name);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 			}
 		}
